d669848547bb7c1360c2f069b0478b81c196e14c
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-09-08 17:25:03 +0200

    Model e Controller creation

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Movie;
+
+class HomeController extends Controller
+{
+    public function index() {
+    
+    }
+}
+
+?>
