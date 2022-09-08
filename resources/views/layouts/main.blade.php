8f04e9bde8387c90b54df56c7c6520e10854629b
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-09-08 18:06:49 +0200

    Model e Controller created + some fixes

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Model Controller</title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+    @include ('layouts.header')
+
+    @yield ('homepage')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index')->name('homepage');
+Route::get('/', function () {
+    return view('homepage');
+});
